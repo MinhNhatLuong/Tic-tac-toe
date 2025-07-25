c194d00b1febf8fcc42923c81420df7eb1c570e3
Luong Minh Nhat <luongminhnhat1604@gmail.com>
2025-07-25 15:56:09 +0700

    Added player's name section

---
+++ src/components/Player.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  };
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{name}</span>
+        ) : (
+          <input type="text" value={name} required />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{!isEditing ? 'Edit' : 'Save'}</button>
+    </li>
+  );
+}
