ee95cb1d0dfaa21cb1167c7c9f3c346c8b03ed0d
Luong Minh Nhat <luongminhnhat1604@gmail.com>
2025-07-26 09:47:16 +0700

    Reducing state management by removed activePlayer state

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,26 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+  if (gameTurns[0]?.player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
   const [gameTurns, setGameTurns] = useState([]);
 
+  const activePlayer = deriveActivePlayer(gameTurns);
+
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((prevActivePlayer) =>
-      prevActivePlayer === "X" ? "O" : "X"
-    );
+    // setActivePlayer((prevActivePlayer) => prevActivePlayer === "X" ? "O" : "X");
     setGameTurns((prevTurns) => {
       // let currentPlayer = prevTurns.length % 2 === 0 ? 'X' : 'O'; //You can use this logic to replace the 2 statements below
 
-      let currentPlayer = 'X';
-
-      // if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-      if (prevTurns[0]?.player === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         {
